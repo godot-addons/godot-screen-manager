4227ef853bd7c8ba44f5923702620814fdf9b8bf
Brandon Lamb <brandon@brandonlamb.com>
2017-12-16 11:10:53 -0800

    Add ConfigFile support

---
+++ addons/godot-screen-manager/config_factory.gd
@@ -0,0 +1,26 @@
+extends Resource
+
+var _file_path
+
+"""
+Constructor accepting resource URL to config file
+"""
+func _init(file_path):
+	_file_path = file_path
+
+"""
+Create configuration using ConfigFile.
+@return dictionary of screen_name to scene path
+"""
+func create():
+	var config_file = ConfigFile.new()
+	if config_file.load(_file_path) != OK:
+		print("Error loading config file")
+		return {}
+
+	var screens = {}
+	for key in config_file.get_section_keys("screens"):
+		print("Screen config: key=", key)
+		screens[key] = str(config_file.get_value("screens", key))
+
+	return screens
--- example/main.gd
+++ example/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const ConfigFactory = preload("res://addons/godot-screen-manager/config_factory.gd")
+
 # Screen manager is an autoloaded singleton (see project settings)
 onready var _screen_manager = get_tree().get_root().get_node("/root/screen_manager")
 
@@ -7,12 +9,11 @@ func _ready():
 	# Set the node path to our screen holding node
 	_screen_manager.set_screen_path("/root/main/screen")
 
-	# Set the available screens
-	_screen_manager.set_screens({
-		"screen1": "res://screen1.tscn",
-		"screen2": "res://screen2.tscn",
-		"screen3": "res://screen3.tscn"
-	})
+	# Configure screens using static dictionary
+	#_init_static()
+
+	# Configure screensing using ConfigFile
+	_init_config_file()
 
 	# On game start, load screen1 as the default
 	load_screen("screen1")
@@ -24,8 +25,21 @@ func load_screen(name):
 	# This could also just connect directly to load_screen for simplicity.
 	# Instead of connecting a signal, the screens could just reference the screen_manager directly as a singleton
 	for i in $screen.get_children():
-		if i.has_user_signal("load_screen"):
-			i.connect("load_screen", self, "_on_load_screen")
+		i.connect("load_screen", self, "_on_load_screen")
 
 func _on_load_screen(name):
 	load_screen(name)
+
+func _init_static():
+	# Set the available screens
+	_screen_manager.set_screens({
+		"screen1": "res://screen1.tscn",
+		"screen2": "res://screen2.tscn",
+		"screen3": "res://screen3.tscn"
+	})
+
+func _init_config_file():
+	var factory = ConfigFactory.new("res://config.cfg")
+	var screens = factory.create()
+	print(screens)
+	_screen_manager.set_screens(screens)
