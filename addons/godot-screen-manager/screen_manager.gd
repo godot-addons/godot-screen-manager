fa5196560e98552d7504fba2c3012c09f7a5160c
Brandon Lamb <brandon@brandonlamb.com>
2017-12-16 10:29:36 -0800

    Add some docs

--- addons/godot-screen-manager/screen_manager.gd
+++ addons/godot-screen-manager/screen_manager.gd
@@ -1,16 +1,25 @@
 extends Node
 
-signal screen_loaded
+signal screen_loaded(name)
 
-var _screen_path
-var _screens
+var _screen_path = NodePath("/root/screen")
+var _screens = {}
 
+"""
+Set the NodePath for the node holding the screen.
+"""
 func set_screen_path(node_path):
 	_screen_path = NodePath(node_path)
 
+"""
+Set the screens dictionary which is key as a screen name and value of the resource URL to the scene.
+"""
 func set_screens(screens):
 	_screens = screens
 
+"""
+Load the screen by it's key name
+"""
 func load_screen(name):
 	print("Load screen: ", name)
 
@@ -31,4 +40,4 @@ func load_screen(name):
 	var next_screen = load(_screens[name]).instance()
 	screen.add_child(next_screen)
 
-	emit_signal("screen_loaded")
+	emit_signal("screen_loaded", name)
--- example/main.gd
+++ example/main.gd
@@ -1,19 +1,31 @@
 extends Node2D
 
+# Screen manager is an autoloaded singleton (see project settings)
 onready var _screen_manager = get_tree().get_root().get_node("/root/screen_manager")
 
 func _ready():
+	# Set the node path to our screen holding node
 	_screen_manager.set_screen_path("/root/main/screen")
+
+	# Set the available screens
 	_screen_manager.set_screens({
 		"screen1": "res://screen1.tscn",
 		"screen2": "res://screen2.tscn",
 		"screen3": "res://screen3.tscn"
 	})
 
+	# On game start, load screen1 as the default
 	load_screen("screen1")
 
 func load_screen(name):
 	_screen_manager.load_screen(name)
 
+	# Example showing us connecting the loaded screen's "load_screen" signal to our handler.
+	# This could also just connect directly to load_screen for simplicity.
+	# Instead of connecting a signal, the screens could just reference the screen_manager directly as a singleton
 	for i in $screen.get_children():
-		i.connect("load_screen", self, "load_screen")
+		if i.has_user_signal("load_screen"):
+			i.connect("load_screen", self, "_on_load_screen")
+
+func _on_load_screen(name):
+	load_screen(name)
